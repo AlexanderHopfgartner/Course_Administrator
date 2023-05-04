b502c5c0be7b9c573fd00fc9d08365e6bcc96d95
rehclem2004 <melcher.christoph@hotmail.com>
2023-05-04 14:51:11 +0200

    functions for validating user entries

--- validator.py
+++ validator.py
@@ -1,1 +1,22 @@
 # TODO 3. create all validation checks
+def validate_name(name):
+    if type(name) == str:
+        return True, "The name is valid"
+    return False, "The name isn't a string, please enter a string"
+def validate_addressname():
+    print("not finished")
+def validate_streetnumber(streetnumber):
+    print("not finished")
+
+def validate_postcode(zipcode):
+    print("not finished")
+
+def validate_city(city):
+    print("not finished")
+
+def validate_TelNr(TelNr):
+    print("not finsihed")
+
+def validate_email(email):
+    print("not finished")
+
