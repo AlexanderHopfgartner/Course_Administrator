2bfb885227d938d9efb1d75f667d29eed1367aca
AlexandereHopfgartner <Alexander.Hopfgartner@outlook.com>
2023-05-08 14:13:57 +0200

    edit displaying

--- course_administrator/member_edit.py
+++ course_administrator/member_edit.py
@@ -1,64 +1,108 @@
 from course_administrator.couse_administrator import Member
+from course_administrator.member_form import get_role
 from consts import INPUT_END
+from logic.validator import *
 
 
 def find_edit_user(db) -> Member | None:
-    edit = input(f"Do you want to edit by \"ID\" or \"full_name\".[\"q!\" ot leave]{INPUT_END}").lower()[0]
-    while edit == "i":
-        edit_user = []
-        edit_key = input(f"Please enter the \"ID\".{INPUT_END}")
-        try:
-            edit_user = [member for member in db.members if member.id == int(edit_key)]
-        except ValueError:
-            print("Sorry your input is not a Valid \"ID\".")
-        if not edit_user:
-            print("Sorry no user with this \"ID\".")
-        elif len(edit_user) == 1:
-            if input(f"Do you want to edit the user: {edit_user}. [yes/no]{INPUT_END}").lower()[0] == "y":
-                edit = edit_user[0]
-        else:
-            [print(f"{edit_user.index(user)}: {user}") for user in edit_user]
-            user = ""
-            while not user:
-                user = input(f"Please enter the correct index of the user{INPUT_END}")
-                try:
-                    edit = edit_user[int(user)]
-                except ValueError:
-                    print("Sorry invalit input.")
-                    user = ""
-                except IndexError:
-                    print("Sorry index not in range.")
-                    user = ""
+    edit = ""
+    while not edit == "!q":
+        edit = input(f"Do you want to edit by \"ID\" or \"full_name\".[\"q!\" ot leave]{INPUT_END}").lower()[0]
+        while edit == "i":
+            edit_user = []
+            edit_key = input(f"Please enter the \"ID\".{INPUT_END}")
+            try:
+                edit_user = [member for member in db.members if member.id == int(edit_key)]
+            except ValueError:
+                print("Sorry your input is not a Valid \"ID\".")
+            if not edit_user:
+                print("Sorry no user with this \"ID\".")
+            elif len(edit_user) == 1:
+                if input(f"Do you want to edit the user: {edit_user}. [yes/no]{INPUT_END}").lower()[0] == "y":
+                    edit = edit_user[0]
+            else:
+                [print(f"{edit_user.index(user)}: {user}") for user in edit_user]
+                user = ""
+                while not user:
+                    user = input(f"Please enter the correct index of the user{INPUT_END}")
+                    try:
+                        edit = edit_user[int(user)]
+                    except ValueError:
+                        print("Sorry invalit input.")
+                        user = ""
+                    except IndexError:
+                        print("Sorry index not in range.")
+                        user = ""
 
-    while edit == "f":
-        edit_user = []
-        edit_key = input(f"Please enter the \"full_name\".{INPUT_END}")
-        edit_user = [member for member in db.members if member.full_name == edit_key]
-        if not edit_user:
-            print("Sorry no user with this \"full_name\".")
-        elif len(edit_user) == 1:
-            if input(f"Do you want to edit the user: {edit_user}. [yes/no]{INPUT_END}").lower()[0] == "y":
-                edit = edit_user[0]
-        else:
-            [print(f"{edit_user.index(user)}: {user}") for user in edit_user]
-            user = ""
-            while not user:
-                user = input(f"Please enter the correct index of the user{INPUT_END}")
-                try:
-                    edit = edit_user[int(user)]
-                except ValueError:
-                    print("Sorry invalit input.")
-                    user = ""
-                except IndexError:
-                    print("Sorry index not in range.")
-                    user = ""
+        while edit == "f":
+            edit_user = []
+            edit_key = input(f"Please enter the \"full_name\".{INPUT_END}")
+            edit_user = [member for member in db.members if member.full_name == edit_key]
+            if not edit_user:
+                print("Sorry no user with this \"full_name\".")
+            elif len(edit_user) == 1:
+                if input(f"Do you want to edit the user: {edit_user}. [yes/no]{INPUT_END}").lower()[0] == "y":
+                    edit = edit_user[0]
+            else:
+                [print(f"{edit_user.index(user)}: {user}") for user in edit_user]
+                user = ""
+                while not user:
+                    user = input(f"Please enter the correct index of the user{INPUT_END}")
+                    try:
+                        edit = edit_user[int(user)]
+                    except ValueError:
+                        print("Sorry invalit input.")
+                        user = ""
+                    except IndexError:
+                        print("Sorry index not in range.")
+                        user = ""
 
-    if type(edit) == Member:
-        return edit
-    if edit == "q!":
-        return
-    print(f"Sorry invalid input.")
+        if type(edit) == Member:
+            return edit
+        print(f"Sorry invalid input.")
+    return
 
 
 def edit(member: Member):
-    pass
+    editing = True
+    while editing:
+        print(member)
+        edit_key = input(f"Please Enter the information want to change\n"
+                         f"[first_name/name/role/address/telnum/email/url]{INPUT_END}").lower()[0]
+        if edit_key == "f":
+            edit_input = input(f"Change {member.first_name} to:{INPUT_END}")
+            if validate_name(edit_input):
+                member.first_name = edit_input
+        if edit_key == "n":
+            edit_input = input(f"Change {member.name} to:{INPUT_END}")
+            if validate_name(edit_input):
+                member.name = edit_input
+        if edit_key == "r":
+            edit_input = get_role()
+            if edit_input:
+                member.role = edit_input
+        while edit_key == "a":
+            edit_key = input(f"{member.display_address()} Please Enter what to change\n"
+                             f"[address_name/street_nummber/postcode/city/q!]{INPUT_END}")
+            if edit_key.lower()[0] == "a":
+                edit_input = input(f"Change {member.address_name} to:{INPUT_END}")
+                if validate_address_name(edit_input):
+                    member.address_name = edit_input
+            if edit_key.lower()[0] == "s":
+                edit_input = input(f"Change {member.street_number} to:{INPUT_END}")
+                if validate_street_number(edit_input):
+                    member.street_number = edit_input
+            if edit_key.lower()[0] == "p":
+                edit_input = input(f"Change {member.postcode} to:{INPUT_END}")
+                if validate_postcode(edit_input):
+                    member.postcode = int(edit_input)
+            if edit_key.lower()[0] == "c":
+                edit_input = input(f"Change {member.city} to:{INPUT_END}")
+                if validate_city(edit_input):
+                    member.city = edit_input
+            if edit_key == "q!":
+                edit_key = ""
+
+
+
+
