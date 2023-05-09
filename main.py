fae57b342dc8a6580ac106a1a6e6c09b859c9764
AlexanderHopfgartner <Alexander.Hopfgartner@outlook.com>
2023-05-09 11:39:37 +0200

    add reloggin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from logic.menu import menu
+from consts import INPUT_END, yon
 
 
 def main():
@@ -6,7 +7,8 @@ def main():
     while run:
         key = menu()
         if key == "q!":
-            run = False
+            if input(f"Logged out\n\nDo you want to log on again?\t{yon}{INPUT_END}").lower()[0] == "n":
+                run = False
     print("Closed")
 
 
