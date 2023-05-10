d22f76db46752d6994eb199a5c719e0d5ff834d7
AlexanderHopfgartner <Alexander.Hopfgartner@outlook.com>
2023-05-10 14:01:06 +0200

    updated Form updated Member.output update user performance

--- course_administrator/couse_administrator.py
+++ course_administrator/couse_administrator.py
@@ -1,7 +1,5 @@
-from consts import INPUT_END, yon
+from consts import INPUT_END, yon, clear, Form, AddressForm, MemberForm
 from logic.validator import *
-from course_administrator.member_form import member_form
-import os
 
 
 class Address(object):
@@ -42,25 +40,22 @@ class Member(Address, object):
         self.output = self.structure()
 
     def display_address(self):
-        return f"|{self.address_name} {self.street_number}, {self.postcode} {self.city}|"
+        return f"{self.address_name:<18} {self.street_number:<6}| {self.postcode:<4} {self.city}"
 
-    def _form(self):
-        return {"telnum": self.telnum,
-                "email": self.email,
-                "url": self.url,
-                "f_name": self.first_name,
-                "name": self.name,
-                "role": self.role,
-                "address":
-                    {
-                        "address_name": self.address_name,
-                        "number": self.street_number,
-                        "postcore": self.postcode,
-                        "city": self.city
-                    },
-                "id": self.id
-
-                }
+    def member_form(self):
+        return MemberForm(
+            telnum=self.telnum,
+            email=self.email,
+            url=self.url,
+            f_name=self.first_name,
+            name=self.name,
+            role=self.role,
+            address=AddressForm(
+                address_name=self.address_name,
+                number=self.street_number,
+                postcode=self.postcode,
+                city=self.city),
+            id=self.id)
 
     def rename(self):
         return self.first_name + " " + self.name
@@ -83,7 +78,7 @@ class Member(Address, object):
                 line += "  Url.: " + str(self.url)
             else:
                 line += "\t\t\t\tUrl.: " + str(self.url)
-        return f"{self.full_name}: {self.role}, lives at {self.display_address()}" + f"{line:>80} "
+        return f"|{self.full_name:<25} | {self.role:<14}, lives at |{self.display_address():<45}|" + f"{line}|"
 
     def __str__(self):
         return self.output
@@ -93,10 +88,6 @@ class Member(Address, object):
 
     def __call__(self, *args, **kwargs):
         return self.output
-
-
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 user_list = []
@@ -116,24 +107,26 @@ def course_administration(db, user) -> str | None:
     #                          f_name="Milch", name="Milchiger", role="Teilnehmer:in",
     #                          address={"address_name": "Nikl", "number": 92, "postcode": 1110,
     #                                   "city": "Wien"}))
-    print(f"welcome in the DataBase administration. {user.full_name}")
+    print(f"\nwelcome in the DataBase administration. <<<{user.full_name}>>>")
     key = ""
     while key != "q!":
         while not key or (not key.isalpha() and not key == "q!"):
-            key = input(f"Enter \"Help\" for help.{INPUT_END}")
+            key = input(f"Current user: <<<{user.full_name}>>>\n\nEnter \"Help\" for help.{INPUT_END}")
         if key.lower()[0] == "h":
             if user.id == 0:
                 print(f"[h/Help]: Help:\tSee all current options.\n\n[a/Add]: Add: Add a member to the DataBase.\n"
                       f"[c/Clear]: Clear\tClears the console.\n[d/Del] Delete:\tRemove a Member by ID form "
                       f"the DataBase.\n[e/Edit] Edit:\t Edit a Member by ID from the DataBase\n"
                       f"[s/Save] Save:\tSave the current Database to the local.\n[v / View]: View:\tshow"
-                      f"all user on the DataBase\n\n[q!] Log out: Logs out the current user")
+                      f"all user on the DataBase\n\n[q!] Log out: Logs out the current user\n")
             else:
                 print(f"[h/Help]: Help:\tSee all current options.\n\n[c/Clear]: Clear\tClears the console.\n[v/View]: "
-                      f"View:\tshow all user.\n\n[q!] Log out: Logs out the current user")
+                      f"View:\tshow all user.\n\n[q!] Log out: Logs out the current user\n")
 
         if key.lower()[0] == "v":
-            [print(member.id, ":\t", member) for member in db.members]
+            print(f"{'_'*220:<10}")
+            [print(f"You >>> {member.id}" if member == user else member.id, ":|\t", member) for member in db.members]
+            print(f"{'_'*220:<10}")
 
         if user.id == 0 and key.lower()[0] == "a":
             """Add a member"""
@@ -167,14 +160,15 @@ def course_administration(db, user) -> str | None:
         if key == "q!":
             clear()
             save_massage = "Did you save, last change!" if user.id == 0 else ""
-            if input(f"{save_massage}\n\n\nDo you want do leave {yon}{INPUT_END}") == "y":
+            if input(f"{save_massage}\n\n\nDo you want do leave [yes?]{INPUT_END}").lower()[0] == "y":
                 return key
             clear()
         key = ""
 
 
 def add_member():
+    from course_administrator.member_form import fill_member_form
     if input(f"Do you want to add a member? {yon}:\n{INPUT_END}", ).lower()[0] == "y":
-        return member_form()
+        return fill_member_form()
     else:
         return
