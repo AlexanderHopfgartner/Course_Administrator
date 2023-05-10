4cc90c400a5c435e9cb721312d1a8a513020a28a
AlexanderHopfgartner <Alexander.Hopfgartner@outlook.com>
2023-05-10 14:02:18 +0200

    remove print, TODO

--- db/logic.py
+++ db/logic.py
@@ -9,16 +9,12 @@ class DB:
             db_read = json.load(db_json)
             self.members += [Member(**data) for data in db_read["ids"]["members"]]
             Member.id = db_read["ids"]["last_id"]
-        # TODO 3.1 by log to the local db set Member.id = db_read.ids.last_id
-
 
     def db_save(self):
-        # TODO 3.2 save all members to a json
-        member_list =[]
+        member_list = []
         for member in self.members:
-            member_list.append(member._form())
-        json_dict = {"ids":{"last_id":Member.id,"members":member_list}}
-        print(json_dict)
+            member_list.append(member.member_form())
+        json_dict = {"ids": {"last_id": Member.id, "members": member_list}}
         with open("db/db.json", 'w') as file_object:  # open the file in write mode
             json.dump(json_dict, file_object)  # json.dump() function to stores data in test.json file
 
