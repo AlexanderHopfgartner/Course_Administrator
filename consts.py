9694f037d9875b048064c0bd399762e7cbbb75dd
AlexanderHopfgartner <Alexander.Hopfgartner@outlook.com>
2023-05-10 13:59:33 +0200

    add TypedDict

--- consts.py
+++ consts.py
@@ -1,2 +1,31 @@
+import os
+from typing import TypedDict
+
+
 INPUT_END = "\n>>>"
 yon = "[yes/no]"
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+class AddressForm(TypedDict):
+    address_name: str
+    number: str
+    postcode: int
+    city: str
+
+
+class Form(TypedDict):
+    telnum: str
+    email: str
+    url: str
+    f_name: str
+    name: str
+    role: str
+    address: AddressForm
+
+
+class MemberForm(TypedDict, Form):
+    id: int
